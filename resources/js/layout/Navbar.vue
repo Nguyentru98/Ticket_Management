3047819208056fdafa5ccb3e2665aa9dc08a9b1d
Tru Nguyen <nguyentruhn@gmail.com>
2024-11-25 15:50:47 +0700

    UI login

--- resources/js/layout/Navbar.vue
+++ resources/js/layout/Navbar.vue
@@ -1,58 +1,91 @@
 <script setup>
-import { RouterLink } from 'vue-router'
+import { RouterLink } from "vue-router";
+
 
 </script>
 
 <template>
-    <div class="container">
-        <nav class="border p-3">
-            <ul>
-                <li>
-                    <img src="/images/logo.png" alt="" style="width: 100px" />
-                </li>
-                <li>
-                    <router-link to="/">Danh sách ticket</router-link>
-                </li>
-                <li>
-                    <router-link to="/createTicket">Tạo Ticket</router-link>
-                </li>
-                <li>
-                    <router-link to="/report">Báo cáo</router-link>
-                </li>
-                <li>
-                <router-link to="/#">Quản trị người dùng</router-link>
-            </li>
+  <div class="container">
+    <nav class="border p-3">
+      <ul>
+        <li>
+          <img src="/images/logo.png" alt="" style="width: 100px" />
+        </li>
+        <li>
+          <router-link to="/">Danh sách ticket</router-link>
+        </li>
+        <li>
+          <router-link to="/createTicket">Tạo Ticket</router-link>
+        </li>
+        <li>
+          <router-link to="/report">Báo cáo</router-link>
+        </li>
+        <li>
+          <router-link to="/#">Quản trị người dùng</router-link>
+        </li>
+      </ul>
+      <div class="d-flex align-items-center">
+        <span class="pi pi-bell"></span>
+      <div class="user">
+        <div class="dropdown">
+          <a
+            class="btn btn-dropdown bg-white dropdown-toggle d-flex align-items-center px-0"
+            href="#"
+            role="button"
+            id="dropdownMenuLink"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+          <span class="px-3">Nguyễn Văn Trụ</span>
+          <span class="pi pi-user"></span>
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
             <li>
-                <router-link to="/loginForm">login</router-link>
+              <a class="dropdown-item">
+                <div class="d-flex align-items-center">
+                  <div class="dropdown-item-icon">
+                  </div>
+                  <span>Thông tin người dùng</span>
+                </div>
+              </a>
             </li>
-            </ul>
-            <div class="d-flex align-items-center">
-                <span class="pi pi-bell"></span>
-                <span class="px-3">Nguyễn Văn Trụ</span>
-                <span class="pi pi-user"></span>
-            </div>
-        </nav>
+           
+            <li>
+              <form method="POST" action="">
+                <button type="submit" class="dropdown-item" href="#">
+                  <div class="d-flex align-items-center">
+                    <div class="dropdown-item-icon">
+                    </div>
+                    <span class="text-item-dropdown">Đăng xuất</span>
+                  </div>
+                </button>
+              </form>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-
+    </nav>
+  </div>
 </template>
 
 <style>
 a {
-    text-decoration: none;
+  text-decoration: none;
 }
 nav {
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    align-items: center;
-    font-weight: bold;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  font-weight: bold;
 }
 li {
-    padding: 0 10px;
+  padding: 0 10px;
 }
 </style>
--- resources/js/pages/auth/LoginPage.vue
+++ resources/js/pages/auth/LoginPage.vue
@@ -1,55 +1,80 @@
 <template>
-    <div>
-        <h1>Login</h1>
-        <form @submit.prevent="handleLogin">
-            <input v-model="email" type="email" placeholder="Email" required />
-            <input v-model="password" type="password" placeholder="Password" required />
-            <button type="submit">Login</button>
-            <button @click="handleLogout">logout</button>
-        </form>
-        <p v-if="error">{{ error }}</p>
-    </div>
+  <div>
+    <form @submit.prevent="handleLogin">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input
+          v-model="email"
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+        <div id="emailHelp" class="form-text">
+          We'll never share your email with anyone else.
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input
+          v-model="password"
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script setup>
-import axios from 'axios';
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+import axios from "axios";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api', // Thay bằng URL của backend
-    withCredentials: true, // Cần thiết để gửi cookie
+  baseURL: "http://127.0.0.1:8000/api", // Thay bằng URL của backend
+  withCredentials: true, // Cần thiết để gửi cookie
 });
 
-const email = ref('');
-const password = ref('');
+const email = ref("");
+const password = ref("");
 const error = ref(null);
 const router = useRouter();
 
 const handleLogin = async () => {
-    try {
-        error.value = null; // Xóa thông báo lỗi
-        await apiClient.post('/login', {
-            email: email.value,
-            password: password.value,
-        });
-        localStorage.setItem('authToken', '1234556');
-        alert('Login successful'); // Hiển thị thông báo thành công
-        router.push('/'); // Chuyển hướng tới dashboard
-    } catch (err) {
-        error.value = err.response?.data?.message || 'Login failed'; // Lưu lỗi
-    }
+  try {
+    error.value = null; // Xóa thông báo lỗi
+    await apiClient.post("/login", {
+      email: email.value,
+      password: password.value,
+    });
+    localStorage.setItem("authToken", "1234556");
+    alert("Login successful"); // Hiển thị thông báo thành công
+    router.push("/"); // Chuyển hướng tới home list
+  } catch (err) {
+    error.value = err.response?.data?.message || "Login failed"; // Lưu lỗi
+  }
 };
 
 const handleLogout = async () => {
-    try {
-        await apiClient.post('/logout'); // Đăng xuất
-        alert('Logout successful'); // Thông báo thành công
-        router.push('/loginForm'); // Chuyển hướng về login
-    } catch (err) {
-        console.error('Logout failed', err); // Log lỗi
-    }
+  try {
+    await apiClient.post("/logout"); // Đăng xuất
+    alert("Logout successful"); // Thông báo thành công
+    router.push("/loginForm"); // Chuyển hướng về login
+  } catch (err) {
+    console.error("Logout failed", err); // Log lỗi
+  }
 };
+
+const logout = ()=> {
+    emit('handleLogout')
+}
 </script>
 
 
